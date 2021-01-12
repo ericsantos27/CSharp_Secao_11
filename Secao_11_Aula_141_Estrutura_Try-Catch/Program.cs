4a7ba921e1dd5a24c310b7d9a9b097f8a65dc32d
ericsantos27 <esantos_samu@hotmail.com>
2021-01-12 10:53:18 -0300

    Exercício para demonstração de tratamento de exceções

---
+++ Secao_11_Aula_141_Estrutura_Try-Catch/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Secao_11_Aula_141_Estrutura_Try_Catch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("1° número: ");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.Write("2° número: ");
+                int num2 = int.Parse(Console.ReadLine());
+                int result = num1 / num2;
+                Console.WriteLine("Resultado: {0}", result);
+            }
+            catch(DivideByZeroException except)
+            {
+                Console.WriteLine("Error !! {0}", except.Message);
+            }
+        }
+    }
+}
