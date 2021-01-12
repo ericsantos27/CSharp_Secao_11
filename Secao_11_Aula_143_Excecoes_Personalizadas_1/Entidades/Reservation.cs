de21dea6fb77fc8bf75111890e0af433ec327b64
ericsantos27 <esantos_samu@hotmail.com>
2021-01-12 14:39:37 -0300

    Projeto para a aula 143 do curso. Solução ruim com if's e else's

---
+++ Secao_11_Aula_143_Excecoes_Personalizadas_1/Entidades/Reservation.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Secao_11_Aula_143_Excecoes_Personalizadas_1.Entidades
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+        public Reservation(int room, DateTime checkIn, DateTime checkOut)
+        {
+            RoomNumber = room;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime inn, DateTime outt)
+        {
+            CheckIn = inn;
+            CheckOut = outt;
+        }
+
+        public override string ToString()
+        {
+            return "Reserva: Quarto " + RoomNumber + "; Check-in: " + CheckIn.ToString("dd/MM/yyyy" ) + "; Check-out: " + CheckOut.ToString("dd/MM/yyyy")
+            + "; Diárias: " + Duration();
+        }
+    }
+}
---
+++ Secao_11_Aula_143_Excecoes_Personalizadas_1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Secao_11_Aula_143_Excecoes_Personalizadas_1.Entidades;
+
+namespace Secao_11_Aula_143_Excecoes_Personalizadas_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quarto: ");
+            int roomNumber = int.Parse(Console.ReadLine());
+            Console.Write("Data de check-in (dd/mm/yyyy): ");
+            DateTime checkInnDate = DateTime.Parse(Console.ReadLine());
+            Console.Write("Data de check-out (dd/mm/yyyy): ");
+            DateTime checkOutDate = DateTime.Parse(Console.ReadLine());
+            
+            if(checkInnDate > checkOutDate)
+            {
+                Console.WriteLine("A data de saída deve ser maior que a data de entrada");
+            }
+            else
+            {
+                Reservation reservationData = new Reservation(roomNumber, checkInnDate, checkOutDate);
+                Console.WriteLine(reservationData);
+
+                Console.WriteLine();
+                Console.WriteLine("Entre com os dados para atualização da reserva: ");
+                Console.Write("Data de check-in (dd/mm/yyyy): ");
+                checkInnDate = DateTime.Parse(Console.ReadLine());
+                Console.Write("Data de check-out (dd/mm/yyyy): ");
+                checkOutDate = DateTime.Parse(Console.ReadLine());
+
+                DateTime today = DateTime.Now;
+                if (checkInnDate < today || checkOutDate < today)
+                {
+                    Console.WriteLine("Alterações de reserva somente para datas futuras");
+                }
+                else if (checkInnDate > checkOutDate)
+                {
+                    Console.WriteLine("A data de saída deve ser maior que a data de entrada");
+                }
+                else
+                {
+                    reservationData.UpdateDates(checkInnDate, checkOutDate);
+                    Console.WriteLine(reservationData);
+                }
+            }
+        }
+    }
+}
